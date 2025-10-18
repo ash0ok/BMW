262b925958fd92317ce2d5591e52d460f7c6530c
ash0ok <ashokyadavashokyadav316@gmail.com>
2025-10-18 10:23:37 +0530

    Add files via upload

---
+++ bmw motor.py
@@ -0,0 +1,14 @@
+class vehicle:
+     def describe(self):
+         print("this is a gernertic vehicle.")
+
+class Car(vehicle):
+      print("this is a Fortuner Car.")
+class Motorcycle(vehicle):
+      def describe(self):
+           print("this is a motorcycle.")
+vehicle=vehicle
+Car=Car
+Motorcycle=Motorcycle
+
+           
